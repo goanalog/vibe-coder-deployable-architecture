e488184d50ce1c0043c070d36fd5253d9567df0d
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 15:08:01 -0500

    asdf

--- inputs.tf
+++ inputs.tf
@@ -1,3 +1,21 @@
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "Your IBM Cloud API key. Required for Terraform to create resources."
+  sensitive   = true
+}
+
+variable "make_public" {
+  type        = bool
+  description = "Whether the bucket should be public."
+  default     = true
+}
+
+variable "html_content" {
+  type        = string
+  description = "Paste your HTML code here to create an instant SPA."
+  default     = "<h1>Welcome to Vibe Coder!</h1><p>Your instant SPA is live.</p>"
+}
+
 variable "resource_group_name" {
   description = "The name of the resource group to deploy resources into."
   type        = string
@@ -13,7 +31,7 @@ variable "bucket_name_prefix" {
 variable "cos_instance_name" {
   description = "The name for the new COS service instance."
   type        = string
-  default     = "vsibe-coder-cos"
+  default     = "vibe-coder-cos"
 }
 
 variable "region" {
