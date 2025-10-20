2ae17fa33a05daf5cae5538ca93f9c58c24a7a46
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 15:06:28 -0500

    asdf

--- inputs.tf
+++ inputs.tf
@@ -1,35 +1,23 @@
-variable "ibmcloud_api_key" {
+variable "resource_group_name" {
+  description = "The name of the resource group to deploy resources into."
   type        = string
-  description = "Your IBM Cloud API key. Required for Terraform to create resources."
-  sensitive   = true
+  default     = "default"
 }
 
-variable "location" {
+variable "bucket_name_prefix" {
+  description = "The prefix for the new COS bucket name."
   type        = string
-  description = "The IBM Cloud location to deploy resources (e.g., us-south, eu-de)."
-  default     = "us-south"
-}
-
-variable "cos_instance_name" {
-  type        = string
-  description = "Name of the IBM Cloud Object Storage instance to create."
-  default     = "vibe-coder-cos"
-}
-
-variable "bucket_name" {
-  type        = string
-  description = "Name of the COS bucket to host your HTML SPA."
   default     = "vibe-coder-sample-bucket"
 }
 
-variable "make_public" {
-  type        = bool
-  description = "Whether the bucket should be public."
-  default     = true
+variable "cos_instance_name" {
+  description = "The name for the new COS service instance."
+  type        = string
+  default     = "vsibe-coder-cos"
 }
 
-variable "html_content" {
+variable "region" {
+  description = "The region to deploy the COS bucket in (e.g., us-south)."
   type        = string
-  description = "Paste your HTML code here to create an instant SPA."
-  default     = "<h1>Welcome to Vibe Coder!</h1><p>Your instant SPA is live.</p>"
+  default     = "us-south"
 }
