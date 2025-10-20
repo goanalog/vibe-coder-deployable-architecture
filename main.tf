0f716b701b1e05691587ad82687762715b6d3429
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 12:09:31 -0500

    asdf

--- main.tf
+++ main.tf
@@ -7,15 +7,16 @@ terraform {
   }
 }
 
-resource "ibm_cos_bucket" "sample" {
-  name                = var.bucket_name
-  cos_instance_id     = ibm_resource_instance.cos.id
-  force_destroy       = true
-  public_access       = var.make_public
+resource "ibm_resource_instance" "cos" {
+  name      = var.cos_name
+  service   = "cloud-object-storage"
+  plan      = "standard"
+  location  = var.region    # <-- add this
 }
 
-resource "ibm_resource_instance" "cos" {
-  name              = var.cos_name
-  service           = "cloud-object-storage"
-  plan              = "standard"
+resource "ibm_cos_bucket" "sample" {
+  bucket      = var.bucket_name        # <-- changed from 'name'
+  instance_id = ibm_resource_instance.cos.id  # <-- changed from 'cos_instance_id'
+  force_destroy = true
+  public_access = var.make_public ? "true" : "false"   # <-- string expected
 }
