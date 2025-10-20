1e354b9219883ce269b72e755b5fa7ad83c878ca
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 14:19:01 -0500

    analog

--- main.tf
+++ main.tf
@@ -15,31 +15,31 @@ data "ibm_iam_access_group" "public_access_group" {
   access_group_name = "Public Access"
 }
 
-# --- THIS IS THE FIX ---
 data "ibm_resource_group" "group" {
   is_default = true
 }
-# ---------------------
 
 resource "ibm_resource_instance" "cos" {
   name              = var.cos_instance_name
   service           = "cloud-object-storage"
   plan              = "standard"
-  location          = var.location
+  # --- THIS IS THE FIX ---
+  location          = "global" 
+  # ---------------------
   resource_group_id = data.ibm_resource_group.group.id
 }
 
 resource "ibm_cos_bucket" "sample" {
   bucket_name          = var.bucket_name
   resource_instance_id = ibm_resource_instance.cos.id
-  region_location      = var.location
+  region_location      = var.location # This is correct, buckets are regional
 }
 
 resource "ibm_iam_access_group_policy" "public_access_policy" {
-  count = var.make_public ? 1 : 0 # Conditionally create this policy
+  count = var.make_public ? 1 : 0 
 
   access_group_id = data.ibm_iam_access_group.public_access_group.id
-  roles           = ["Content Reader"] # Role for public web hosting
+  roles           = ["Content Reader"] 
 
   resources {
     service              = "cloud-object-storage"
@@ -51,7 +51,7 @@ resource "ibm_iam_access_group_policy" "public_access_policy" {
 
 resource "ibm_cos_bucket_object" "html_spa" {
   bucket_crn      = ibm_resource_instance.cos.crn
-  bucket_location = var.location
+  bucket_location = var.location # This is also correct
   key             = "index.html"
   content         = var.html_content
 
