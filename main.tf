fec73227cdfa74534eb2618e979c81b45467123a
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 13:16:47 -0500

    asdf

--- main.tf
+++ main.tf
@@ -11,10 +11,11 @@ provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
 }
 
-# We need this to create the IAM policy
+# --- THIS IS THE FIX ---
 data "ibm_iam_access_group" "public_access_group" {
-  name = "Public Access"
+  access_group_name = "Public Access"
 }
+# ---------------------
 
 data "ibm_resource_group" "group" {
   name = var.resource_group_name
@@ -31,8 +32,6 @@ resource "ibm_resource_instance" "cos" {
 resource "ibm_cos_bucket" "sample" {
   bucket_name          = var.bucket_name
   resource_instance_id = ibm_resource_instance.cos.id
-  # --- FIX 2: REMOVED force_destroy and public_access ---
-  # These arguments are not valid for ibm_cos_bucket
 }
 
 resource "ibm_iam_access_group_policy" "public_access_policy" {
@@ -50,13 +49,11 @@ resource "ibm_iam_access_group_policy" "public_access_policy" {
 }
 
 resource "ibm_cos_bucket_object" "html_spa" {
-  # Argument 'bucket' is the name, not the resource ID
   bucket       = ibm_cos_bucket.sample.bucket_name
   key          = "index.html"
   content      = var.html_content
   content_type = "text/html"
 
-  # Ensures the bucket is public before trying to upload
   depends_on = [
     ibm_iam_access_group_policy.public_access_policy
   ]
