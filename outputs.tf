1427d552cd72aa5bc73499d35ed29048813e38fb
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 10:32:23 -0500

    test

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,7 @@
 output "bucket_name" {
-  value       = ibm_cos_bucket.bucket.name
-  description = "The COS bucket hosting the sample app."
+  value = ibm_cos_bucket.sample_app_bucket.bucket
 }
 
 output "app_url" {
-  value       = "https://${ibm_cos_bucket.bucket.name}.s3.${var.region}.cloud-object-storage.appdomain.cloud/index.html"
-  description = "URL for accessing the sample app."
+  value = "https://${ibm_cos_bucket.sample_app_bucket.bucket}.s3.${var.region}.cloud-object-storage.appdomain.cloud/index.html"
 }
--- schematics.tfvars
+++ schematics.tfvars
@@ -1,3 +1,5 @@
 ibmcloud_api_key = "<YOUR_API_KEY>"
-cos_crn          = "<YOUR_COS_CRN>"
 region           = "us-south"
+cos_crn          = "<YOUR_COS_CRN>"
+bucket_name      = "vibe-coder-sample-bucket"
+make_public      = true
