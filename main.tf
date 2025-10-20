ba8d3ab3b69941441ba1a42c7fd070f198427966
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 13:13:09 -0500

    asdf

--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
 }
 
+# We need this to create the IAM policy
+data "ibm_iam_access_group" "public_access_group" {
+  name = "Public Access"
+}
+
 data "ibm_resource_group" "group" {
   name = var.resource_group_name
 }
@@ -24,17 +29,35 @@ resource "ibm_resource_instance" "cos" {
 }
 
 resource "ibm_cos_bucket" "sample" {
-  # --- THIS IS THE FIX ---
-  bucket_name          = var.bucket_name 
-  # ---------------------
+  bucket_name          = var.bucket_name
   resource_instance_id = ibm_resource_instance.cos.id
-  force_destroy        = true
-  public_access        = var.make_public
+  # --- FIX 2: REMOVED force_destroy and public_access ---
+  # These arguments are not valid for ibm_cos_bucket
 }
 
-resource "ibm_cos_object" "html_spa" {
+resource "ibm_iam_access_group_policy" "public_access_policy" {
+  count = var.make_public ? 1 : 0 # Conditionally create this policy
+
+  access_group_id = data.ibm_iam_access_group.public_access_group.id
+  roles           = ["Content Reader"] # Role for public web hosting
+
+  resources {
+    service              = "cloud-object-storage"
+    resource_instance_id = ibm_resource_instance.cos.id
+    resource_type        = "bucket"
+    resource             = ibm_cos_bucket.sample.bucket_name
+  }
+}
+
+resource "ibm_cos_bucket_object" "html_spa" {
+  # Argument 'bucket' is the name, not the resource ID
   bucket       = ibm_cos_bucket.sample.bucket_name
   key          = "index.html"
   content      = var.html_content
   content_type = "text/html"
+
+  # Ensures the bucket is public before trying to upload
+  depends_on = [
+    ibm_iam_access_group_policy.public_access_policy
+  ]
 }
