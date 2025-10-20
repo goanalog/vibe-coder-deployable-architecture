4e90878d884b7ecfac290a0bc5b168fcc0e1ddcf
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 11:21:11 -0500

    fix

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
+resource "ibm_resource_instance" "cos" {
+  name       = var.cos_name
+  service    = "cloud-object-storage"
+  plan       = "standard"
+  resource_group_id = data.ibm_resource_group.default.id
+}
+
 resource "ibm_cos_bucket" "sample" {
-  bucket = var.bucket_name
-  cos_instance = ibm_resource_instance.cos.id
+  bucket        = var.bucket_name
+  cos_instance  = ibm_resource_instance.cos.id
   public_access = var.make_public
 }
 
@@ -9,3 +16,10 @@ resource "local_file" "sample_html" {
   filename = "${path.module}/sample-app-output.html"
 }
 
+output "bucket_name" {
+  value = ibm_cos_bucket.sample.bucket
+}
+
+output "app_url" {
+  value = "https://${var.bucket_name}.s3.${var.region}.cloud-object-storage.appdomain.cloud/index.html"
+}
