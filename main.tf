25665bbd852e0d8b5857de05a51039f85880f020
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 20:11:41 -0500

    asdf

--- main.tf
+++ main.tf
@@ -3,21 +3,19 @@ provider "ibm" {
   region           = var.location
 }
 
-resource "ibm_resource_group" "vibe_group" {
-  name = "Vibe-Coder-RG"
+resource "ibm_cos_bucket" "vibe_spa" {
+  bucket = "vibe-coder-spa-${random_id.bucket_id.hex}"
+  acl    = var.make_public ? "public-read" : "private"
 }
 
-resource "ibm_cos_bucket" "spa_bucket" {
-  name          = "vibe-coder-spa-bucket"
-  location      = var.location
-  resource_group = ibm_resource_group.vibe_group.id
-  force_destroy = true
-  public_access = var.make_public ? "public-read" : "private"
+resource "random_id" "bucket_id" {
+  byte_length = 4
 }
 
 resource "ibm_cos_bucket_object" "html_spa" {
-  bucket       = ibm_cos_bucket.spa_bucket.name
-  key          = "index.html"
-  source       = "${path.module}/default.html"
+  bucket      = ibm_cos_bucket.vibe_spa.bucket
+  key         = "index.html"
+  source      = null
+  content     = var.vibe_code
   content_type = "text/html"
 }
