230c135493146d65fcd86db2e6f31b25123b673b
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 22:26:40 -0500

    asdf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = ">= 1.84.0"
+    }
+  }
+  required_version = ">= 1.2"
+}
+
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.cos_bucket_location
