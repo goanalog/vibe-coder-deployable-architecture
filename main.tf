37a68afddfdd4089dbfb83bd11c68864b3f3c3db
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 22:23:33 -0500

    asdf

--- main.tf
+++ main.tf
@@ -1,75 +1,17 @@
-terraform {
-  required_version = ">= 1.12"
-  required_providers {
-    ibm = {
-      source  = "IBM-Cloud/ibm"
-      version = ">= 1.84.0"
-    }
-  }
-}
-
-# -------------------------
-# Variables
-# -------------------------
-variable "cos_bucket_name" {
-  description = "Name your IBM Cloud Object Storage bucket. Don't worry, it just needs to be unique in your region."
-  type        = string
-  default     = "vibe-coder-sample-bucket"
-}
-
-variable "cos_region" {
-  description = "Region where your COS bucket will be created. Example: us-south"
-  type        = string
-  default     = "us-south"
-}
-
-# -------------------------
-# IBM COS Bucket
-# -------------------------
-resource "ibm_cos_bucket" "vibe_spa_bucket" {
-  bucket_name = var.cos_bucket_name
-  location    = var.cos_region
-  force       = true  # allows re-creating if bucket exists
-}
-
-# -------------------------
-# SPA HTML Object
-# -------------------------
-resource "ibm_cos_bucket_object" "html_spa" {
-  bucket          = var.cos_bucket_name
-  bucket_location = var.cos_region
-  key             = "index.html"   # the name in the bucket
-  source          = "index.html"   # local file to upload
-  content_type    = "text/html"
-}
-
-# -------------------------
-# Optional Output
-# -------------------------
-output "spa_url" {
-  value       = "https://${var.cos_bucket_name}.s3.${var.cos_region}.cloud-object-storage.appdomain.cloud/index.html"
-  description = "URL to access your deployed SPA."
-}
-terraform {
-  required_version = ">= 1.12"
-  required_providers {
-    ibm = {
-      source  = "IBM-Cloud/ibm"
-      version = ">= 1.84.0"
-    }
-  }
+provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.cos_bucket_location
 }
 
 resource "ibm_cos_bucket" "vibe_spa_bucket" {
   bucket_name = var.cos_bucket_name
-  location    = var.cos_region
-  force       = true
+  location    = var.cos_bucket_location
+  force_destroy = true
 }
 
 resource "ibm_cos_bucket_object" "html_spa" {
-  bucket          = var.cos_bucket_name
-  bucket_location = var.cos_region
-  key             = "index.html"
-  source          = "index.html"
-  content_type    = "text/html"
+  bucket_crn = ibm_cos_bucket.vibe_spa_bucket.bucket_crn
+  key        = "index.html"
+  content    = var.html_content
+  content_type = "text/html"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
+output "cos_bucket_name" {
+  value       = ibm_cos_bucket.vibe_spa_bucket.bucket_name
+  description = "The name of the COS bucket hosting your SPA"
+}
+
 output "spa_url" {
-  value       = "https://${var.cos_bucket_name}.s3.${var.cos_region}.cloud-object-storage.appdomain.cloud/index.html"
-  description = "URL to access your deployed SPA."
+  value       = "https://${ibm_cos_bucket.vibe_spa_bucket.bucket_name}.s3.${var.cos_bucket_location}.cloud-object-storage.appdomain.cloud/index.html"
+  description = "URL to access your deployed SPA"
 }
