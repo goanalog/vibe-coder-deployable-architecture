3975d30a5b2ed3db95e8ee3e7c41f396bdc1cc4d
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 23:47:39 -0500

    f

--- main.tf
+++ main.tf
@@ -11,19 +11,30 @@ provider "ibm" {
   ibmcloud_api_key = var.api_key
 }
 
+# --- NEW RESOURCE ---
+# This creates the Cloud Object Storage service instance
+resource "ibm_resource_instance" "cos_instance" {
+  name              = var.cos_instance_name
+  service           = "cloud-object-storage"
+  plan              = var.cos_plan
+  location          = var.cos_bucket_location # Use the same location for the instance
+  resource_group_id = var.resource_group_id
+}
+# --------------------
+
 locals {
   spa_content = var.pasted_code
 }
 
 resource "ibm_cos_bucket" "vibe_spa_bucket" {
   bucket               = var.cos_bucket_name
-  resource_instance_id = var.cos_instance_id
+  # --- UPDATED LINE ---
+  # This now refers to the new resource we just created
+  resource_instance_id = ibm_resource_instance.cos_instance.id 
   location             = var.cos_bucket_location
 }
 
 resource "ibm_cos_bucket_object" "vibe_index" {
-  # --- ADD THIS LINE ---
-  # Only create this resource if var.pasted_code is not empty
   count = length(var.pasted_code) > 0 ? 1 : 0
 
   bucket_location = ibm_cos_bucket.vibe_spa_bucket.location
@@ -33,8 +44,6 @@ resource "ibm_cos_bucket_object" "vibe_index" {
 }
 
 output "spa_url" {
-  # Updated value to be conditional
   value = length(var.pasted_code) > 0 ? "https://${ibm_cos_bucket.vibe_spa_bucket.bucket}.s3.${var.cos_bucket_location}.cloud-object-storage.appdomain.cloud/index.html" : "No HTML provided. Bucket was created, but no SPA is deployed."
-  
   description = "Key URL to access your deployed SPA"
 }
--- variables.tf
+++ variables.tf
@@ -1,16 +1,29 @@
 variable "cos_bucket_name" {
-  description = "Name your IBM Cloud Object Storage bucket."
+  description = "Name for your new IBM Cloud Object Storage bucket."
   type        = string
   default     = "vibe-coder-sample-bucket"
 }
 
-variable "cos_instance_id" {
-  description = "IBM COS service instance ID. Required to create the bucket."
+variable "cos_instance_name" {
+  description = "Name for your new IBM Cloud Object Storage service instance."
   type        = string
+  default     = "vibe-coder-cos-instance"
+}
+
+variable "resource_group_id" {
+  description = "The ID of the Resource Group where the COS instance will be created."
+  type        = string
+  # No default, this is a required input
+}
+
+variable "cos_plan" {
+  description = "The plan for the COS service instance."
+  type        = string
+  default     = "standard" # 'standard' is the most common plan
 }
 
 variable "cos_bucket_location" {
-  description = "COS bucket location/region. Default is 'us-south'."
+  description = "Region where the COS instance and bucket will be created."
   type        = string
   default     = "us-south"
 }
@@ -18,9 +31,7 @@ variable "cos_bucket_location" {
 variable "pasted_code" {
   description = "Optional: Paste your SPA code (HTML) here."
   type        = string
-  default     = "" # Changed from HTML block to empty string
-  
-  # Validation block removed to make it optional
+  default     = ""
 }
 
 variable "api_key" {
