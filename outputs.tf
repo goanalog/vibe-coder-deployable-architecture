a5752f332825b860631683af6085e56db52168ca
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 22:52:01 -0500

    asdf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cos_bucket_name" {
-  description = "The name of the IBM COS bucket hosting your SPA"
-  value       = ibm_cos_bucket.vibe_spa_bucket.name
-}
-
-output "spa_url" {
-  description = "The URL to access your Vibe Coder SPA"
-  value       = "https://${ibm_cos_bucket.vibe_spa_bucket.name}.s3.${var.cos_bucket_location}.cloud-object-storage.appdomain.cloud/index.html"
-}
