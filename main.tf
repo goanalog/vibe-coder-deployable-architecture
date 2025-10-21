4d7055552a6d8124adbe0f558423e8caa0f716ed
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 21:25:17 -0500

    adf

--- main.tf
+++ main.tf
@@ -9,28 +9,37 @@ terraform {
 
 provider "ibm" {}
 
+# Input variable (inline)
+variable "vibe_code" {
+  type        = string
+  description = "Paste your HTML code here. This drives the Vibe Coder app!"
+  default     = "<!DOCTYPE html><html><body><h1>Hello, Vibe!</h1></body></html>"
+}
+
 # Resource group
 resource "ibm_resource_group" "vibe_rg" {
   name = "vibe-coder-rg"
 }
 
-# COS instance
+# Cloud Object Storage instance
 resource "ibm_resource_instance" "vibe_cos" {
-  name            = "vibe-coder-cos"
-  service         = "cloud-object-storage"
-  plan            = "standard"
-  resource_group  = ibm_resource_group.vibe_rg.id
+  name           = "vibe-coder-cos"
+  service        = "cloud-object-storage"
+  plan           = "standard"
+  resource_group = ibm_resource_group.vibe_rg.id
+  location       = "us-south"
 }
 
 # Bucket
 resource "ibm_cos_bucket" "vibe_spa_bucket" {
   cos_instance_id = ibm_resource_instance.vibe_cos.id
+  bucket          = "vibe-coder-bucket"
 }
 
-# Initial SPA HTML object
+# Upload HTML from input
 resource "ibm_cos_bucket_object" "html_spa" {
-  bucket      = ibm_cos_bucket.vibe_spa_bucket.name
-  key         = "index.html"
-  content     = file("${path.module}/default_index.html")
+  bucket       = ibm_cos_bucket.vibe_spa_bucket.name
+  key          = "index.html"
+  content      = var.vibe_code
   content_type = "text/html"
 }
