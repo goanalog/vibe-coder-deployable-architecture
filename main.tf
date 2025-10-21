77dd719bf20bdf34c889f3d5deb8bbca65a387c4
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 21:17:24 -0500

    adf

--- main.tf
+++ main.tf
@@ -7,33 +7,29 @@ terraform {
   }
 }
 
-provider "ibm" {}
+provider "ibm" {
+}
 
-# Resource group
 resource "ibm_resource_group" "vibe_rg" {
-  name = "Vibe Coder RG"
+  name = "vibe-coder-rg"
 }
 
-# Cloud Object Storage instance
 resource "ibm_resource_instance" "vibe_cos" {
-  name     = "vibe-coder-cos"
-  service  = "cloud-object-storage"
-  plan     = "standard"
-  location = "us-south"
-  tags     = ["vibe-coder"]
+  name             = "vibe-coder-cos"
+  service          = "cloud-object-storage"
+  plan             = "standard"
+  resource_group_id = ibm_resource_group.vibe_rg.id
+  location         = "us-south"
 }
 
-# COS bucket
 resource "ibm_cos_bucket" "vibe_spa_bucket" {
-  bucket_name          = "vibe-coder-spa"
-  resource_instance_id = ibm_resource_instance.vibe_cos.id
+  bucket = "vibe-coder-sample-bucket"
+  cos_instance_id = ibm_resource_instance.vibe_cos.id
 }
 
-# Upload user HTML
 resource "ibm_cos_bucket_object" "html_spa" {
-  bucket_crn     = ibm_cos_bucket.vibe_spa_bucket.bucket_crn
-  bucket_location = ibm_cos_bucket.vibe_spa_bucket.location
-  key            = "index.html"
-  content        = var.vibe_code
-  content_type   = "text/html"
+  bucket       = ibm_cos_bucket.vibe_spa_bucket.bucket
+  key          = "index.html"
+  content      = var.vibe_code
+  content_type = "text/html"
 }
