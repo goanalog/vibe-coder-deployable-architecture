f00ceeafc2b00075c894212af64afe131701963c
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 20:24:09 -0500

    asdf

--- main.tf
+++ main.tf
@@ -3,21 +3,24 @@ provider "ibm" {
   region           = var.location
 }
 
-resource "ibm_resource_group" "default" {
-  name = "vibe-coder-rg"
+resource "ibm_cos_bucket" "spa_bucket" {
+  bucket = "vibe-coder-sample-bucket"
+  location = var.location
+  force_destroy = true
 }
 
-resource "ibm_cos_bucket" "spa_bucket" {
-  name            = "vibe-coder-spa-bucket"
-  location        = var.location
-  resource_group  = ibm_resource_group.default.id
-  force_destroy   = true
-  public_access   = var.make_public
+# Generate the full SPA HTML using a template
+data "template_file" "spa_html" {
+  template = file("${path.module}/spa.tpl.html")
+  vars = {
+    vibe_code = var.vibe_code
+  }
 }
 
 resource "ibm_cos_bucket_object" "html_spa" {
   bucket       = ibm_cos_bucket.spa_bucket.name
   key          = "index.html"
-  content      = var.vibe_code
+  content      = data.template_file.spa_html.rendered
   content_type = "text/html"
+  acl          = var.make_public ? "public-read" : "private"
 }
