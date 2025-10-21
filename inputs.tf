5e422b9921bf7e2ed0bb44ebf8e0675b823a623e
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 20:05:43 -0500

    asdf

--- inputs.tf
+++ inputs.tf
@@ -1,19 +1,21 @@
 variable "ibmcloud_api_key" {
   type        = string
-  description = "Your IBM Cloud API key"
+  description = "Your IBM Cloud API Key"
 }
 
 variable "location" {
-  type    = string
-  default = "us-south"
+  type        = string
+  default     = "us-south"
+  description = "IBM Cloud region to deploy resources"
 }
 
 variable "make_public" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "If true, the SPA will be publicly accessible"
 }
 
 variable "vibe_code" {
   type        = string
-  description = "HTML content for the SPA"
+  description = "HTML code for the live-editable SPA"
 }
