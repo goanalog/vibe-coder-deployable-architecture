c0f6a4eb11cb31ec4bad7f168b4d5d8e57e55d56
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 21:55:32 -0500

    asdf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "api_key" {
-  description = "Your IBM Cloud API key for provisioning COS resources."
-  type        = string
-}
-
-variable "vibe_code" {
-  description = "The HTML code for your Vibe Coder single-page app."
-  type        = string
-}
