75f2d71753509a73eca0195205627c55cc04f8e6
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-20 21:21:57 -0500

    adsf

--- main.tf
+++ main.tf
@@ -7,29 +7,30 @@ terraform {
   }
 }
 
-provider "ibm" {
-}
+provider "ibm" {}
 
+# Resource group
 resource "ibm_resource_group" "vibe_rg" {
   name = "vibe-coder-rg"
 }
 
+# COS instance
 resource "ibm_resource_instance" "vibe_cos" {
-  name             = "vibe-coder-cos"
-  service          = "cloud-object-storage"
-  plan             = "standard"
-  resource_group_id = ibm_resource_group.vibe_rg.id
-  location         = "us-south"
+  name            = "vibe-coder-cos"
+  service         = "cloud-object-storage"
+  plan            = "standard"
+  resource_group  = ibm_resource_group.vibe_rg.id
 }
 
+# Bucket
 resource "ibm_cos_bucket" "vibe_spa_bucket" {
-  bucket = "vibe-coder-sample-bucket"
   cos_instance_id = ibm_resource_instance.vibe_cos.id
 }
 
+# Initial SPA HTML object
 resource "ibm_cos_bucket_object" "html_spa" {
-  bucket       = ibm_cos_bucket.vibe_spa_bucket.bucket
-  key          = "index.html"
-  content      = var.vibe_code
+  bucket      = ibm_cos_bucket.vibe_spa_bucket.name
+  key         = "index.html"
+  content     = file("${path.module}/default_index.html")
   content_type = "text/html"
 }
